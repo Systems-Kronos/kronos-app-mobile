2f280b532cca405c62ebe1ebbdfd1c313b031fdc
Camilla Moreno <camilla.almeida@germinare.org.br>
2025-10-11 01:29:29 -0300

    fix: main

--- app/src/main/java/com/example/kronosprojeto/MainActivity.java
+++ app/src/main/java/com/example/kronosprojeto/MainActivity.java
@@ -1,10 +1,13 @@
+
 package com.example.kronosprojeto;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -24,8 +27,11 @@ import com.example.kronosprojeto.dto.UserResponseDto;
 import com.example.kronosprojeto.model.Notification;
 import com.example.kronosprojeto.service.NotificationService;
 import com.example.kronosprojeto.service.UserService;
+import com.example.kronosprojeto.ui.Login.LoginActivity;
+import com.example.kronosprojeto.ui.SplashScreen.SplashScreen;
 import com.example.kronosprojeto.utils.NotificationHelper;
 import com.example.kronosprojeto.utils.NotificationProcessor;
+import com.example.kronosprojeto.utils.ToastHelper;
 import com.example.kronosprojeto.viewmodel.NotificationViewModel;
 import com.example.kronosprojeto.viewmodel.UserViewModel;
 import com.example.kronosprojeto.workers.NotificationWorker;
@@ -57,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         View customView = getLayoutInflater().inflate(R.layout.toolbar_custom, toolbar, false);
+
         toolbar.addView(customView);
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
@@ -67,12 +74,16 @@ public class MainActivity extends AppCompatActivity {
                 R.id.PerfilFragment,
                 R.id.ChatFragment,
                 R.id.NotificationsFragment,
-                R.id.assignmentHistoryFragment
+                R.id.assignmentHistoryFragment,
+                R.id.details
         ).build();
 
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
+
+
         BottomNavigationView navView = binding.navView;
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
         ImageView notificationIcon = customView.findViewById(R.id.notification_logo);
@@ -106,11 +117,26 @@ public class MainActivity extends AppCompatActivity {
                                 .putString("id", String.valueOf( response.body().getId()))
                                 .apply();
                         carregarNotificacoes(response.body().getId());
+                        TextView title = customView.findViewById(R.id.toolbar_title);
+                        title.setText("Olá, "+ response.body().getNome().split(" ")[0]+ ", como você está? ");
+                        toolbar.removeView(customView);
+                        toolbar.addView(customView);
+
+
+
+                    }else{
+                        if (response.code() == 403){
+                            Intent intent = new Intent(MainActivity.this, SplashScreen.class);
+                            startActivity(intent);
+
+                        }
                     }
                 }
 
                 @Override
                 public void onFailure(Call<UserResponseDto> call, Throwable t) {
+                    ToastHelper.showFeedbackToast(getApplicationContext(),"error","ERRO:","Não foi possível carregar as tarefas");
+
                 }
             });
         }
@@ -142,7 +168,10 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onFailure(Call<List<Notification>> call, Throwable t) {}
+            public void onFailure(Call<List<Notification>> call, Throwable t) {
+                ToastHelper.showFeedbackToast(getApplicationContext(),"error","ERRO:","Não foi carregar as notificações");
+
+            }
         });
         NotificationHelper.createNotificationChannel(this);
         PeriodicWorkRequest notificationWorkRequest =
